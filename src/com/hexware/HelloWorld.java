75aa76674240169149ce28782753be4853ef5af4
Diwakar <hexstu009@sdettraining.com>
2016-04-05 10:32:42 -0400

    First time checkin

---
+++ src/com/hexware/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.hexware;
+
+class HelloWorld
+{
+	public void sayHi ()
+	{
+		System.out.println("Hi from git to jenkins");
+	}
+
+	public static void main(String args[])
+	{
+		new HelloWorld().sayHi();
+	}
+}
